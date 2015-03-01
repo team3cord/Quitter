b5d684cf490c6010c20eacad96febed6bd9546f2
MattCordeiro <matthew3cord@gmail.com>
2015-02-28 23:18:48 -0800

    Syntax error, still issues with get request.

--- node/server.js
+++ node/server.js
@@ -5,12 +5,14 @@ var Post = require('../models/post');
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/posts', function(req, res, next){
-    Post.find(function(err, posts){
-        if(err) {return next(err)}
+app.get('/api/posts', function(req, res, next) {
+    Post.find(function (err, posts) {
+        if (err) {
+            return next(err)
+        }
         res.json(posts);
     });
-);
+});
 app.post('/api/posts', function(req, res, next){
     var post = new Post({
         username : req.body.username,

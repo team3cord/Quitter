e77b0a90dab7ff77ef8ff9bbd2231c91b35ccb63
MattCordeiro <matthew3cord@gmail.com>
2015-03-02 22:30:08 -0800

    Breaking app.js into services PostsSvc => posting empty posts.

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+var quitterApp = angular.module('quitterApp', []);
+
+quitterApp.controller('PostsCtrl', function ($scope, PostsSvc) {
+    $scope.addPost = function () {
+        if ($scope.postBody) {
+            PostsSvc.create({
+                username: "Default User",
+                body: $scope.postBody
+            })
+                .success(function (post) {
+                    $scope.posts.unshift(post)
+                    $scope.postBody = null;
+                });
+        }
+    }
+    PostsSvc.fetch()
+        .success(function(posts){
+            $scope.posts = posts
+        });
+
+});
+quitterApp.service('PostsSvc', function($http){
+    this.fetch = function(){
+        return $http.get('http://localhost:3000/api/posts')
+    }
+    this.create = function(){
+        return $http.post('http://localhost:3000/api/posts')
+    }
+});
--- controllers/static.js
+++ controllers/static.js
@@ -1,7 +1,10 @@
-var router = require('express').Router();
+var express = require('express');
+var router = express.Router();
 
 router.get('/', function(req, res){
     res.sendFile('/Users/matthewcordeiro/Desktop/PCS-projects/Quitter/layouts/posts.html');
 });
+
+router.use(express.static(__dirname + '/../assets'));
 
 module.exports = router;

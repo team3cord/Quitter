2bf5195f5123c8b878ed52e5e3434790c8d3fd40
MattCordeiro <matthew3cord@gmail.com>
2015-03-03 20:44:24 -0800

    Logging.

--- assets/app.js
+++ assets/app.js
@@ -24,6 +24,7 @@ quitterApp.service('PostsSvc', function($http){
         return $http.get('http://localhost:3000/api/posts');
     }
     this.create = function(post){
+        console.log(post);
         return $http.post('http://localhost:3000/api/posts', post);
     }
 });

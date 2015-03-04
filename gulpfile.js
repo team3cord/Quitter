31a694098a346b06871b7a6047356df3f9e99027
MattCordeiro <matthew3cord@gmail.com>
2015-03-03 21:38:02 -0800

    Added gulp & changed dir structure.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('welcome', function(){
+    console.log('Welcome to Gulp!');
+});
+
+gulp.task('hello',['welcome'], function () {
+    console.log('Hello World.');
+});
+
+gulp.task('js', function(){
+    gulp.src('ng/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('assets'));
+});

46b34497d8149dd41537ef9c2c5a4c65792adb38
MattCordeiro <matthew3cord@gmail.com>
2015-03-05 20:48:10 -0800

    Adde first gulp watcher for JS changes.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,10 @@ gulp.task('hello',['welcome'], function () {
 gulp.task('js', function(){
     gulp.src(['ng/module.js','ng/**/*.js'])
         .pipe(concat('app.js'))
-        .pipe(ngAnnotate)
+        .pipe(ngAnnotate())
         .pipe(uglify())
         .pipe(gulp.dest('assets'));
+});
+gulp.task('watch:js', ['js'], function(){
+    gulp.watch('ng/**/*.js', ['js']);
 });

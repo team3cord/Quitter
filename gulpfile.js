c0d99096aae2133293a7e8bea241ec8af8c568bc
MattCordeiro <matthew3cord@gmail.com>
2015-03-05 20:41:18 -0800

    Added gulp-ng-annotate to solve minification/dependency injection issues.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+
 
 gulp.task('welcome', function(){
     console.log('Welcome to Gulp!');
@@ -12,5 +15,7 @@ gulp.task('hello',['welcome'], function () {
 gulp.task('js', function(){
     gulp.src(['ng/module.js','ng/**/*.js'])
         .pipe(concat('app.js'))
+        .pipe(ngAnnotate)
+        .pipe(uglify())
         .pipe(gulp.dest('assets'));
 });

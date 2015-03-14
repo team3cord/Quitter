4280355de94b3521a9a23ea1b5e108361f62a00e
MattCordeiro <matthew3cord@gmail.com>
2015-03-14 15:30:59 -0700

    Post & get jwt auth working w/ curl not for postman.

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,12 +4,12 @@ var //User = require('../models/user.js'),
 
 var secretKey = 'supersecretkey';
 
-router.post('/session',function(req,res){
+router.post('/session',function(req, res){
     var username = req.body.username,
         token = jwt.encode({username: username}, secretKey);
     res.json(token);
 });
-router.get('/user', function(){
+router.get('/user', function(req, res){
    var token = req.headers['x-auth'],
        user = jwt.decode(token, secretKey);
     res.json(user);

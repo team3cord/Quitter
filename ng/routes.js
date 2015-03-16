d445f4a9261421de2bcdc32a2617cc08cc9be8af
MattCordeiro <matthew3cord@gmail.com>
2015-03-16 11:29:22 -0700

    Moved templates to public => added ng-view for ng-routes.

--- ng/routes.js
+++ ng/routes.js
@@ -1,7 +1,7 @@
-angular.module('app')
+angular.module('quitterApp')
     .config(function ($routeProvider){
         $routeProvider
             .when('/',         {controller: 'PostsCtrl', templateUrl: '/templates/posts.html'})
             .when('/register', {controller: 'RegisterCtrl', templateUrl: '/templates/register.html'})
             .when('/login',    {controller: 'LoginCtrl', templateUrl: '/templates/login.html'})
-    });
+});

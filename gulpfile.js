3daf8e0f3fdb9f5f18a9a2c916f55b55c8a25ea1
MattCordeiro <matthew3cord@gmail.com>
2015-03-24 15:12:35 -0700

    Reverted away from ctrl as syntax.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('js', function(){
     gulp.src(['ng/module.js','ng/**/*.js'])
         .pipe(concat('app.js'))
         .pipe(ngAnnotate())
-        .pipe(uglify())
+        //.pipe(uglify())
         .pipe(gulp.dest('public'));
 });
 gulp.task('watch:js', ['js'], function(){
